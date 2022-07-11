987473f6cd2d8a99e8f107f7f290057f2612416e
Ciusii <ciusivinicius@hotmail.com>
2022-07-11 17:06:44 -0300

    Customer Controller

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,14 +1,16 @@
 package com.mercadolivro.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController //Falando para o Spring que essa classe eh uma controller
+@RequestMapping("customer") //declarando o caminho que o controllador eh referente
 class CustomerController {
 
     //endpoint get
     @GetMapping
     fun helloWorld(): String {
-        return "Hello World"
+        return "Customer 1"
     }
 }
